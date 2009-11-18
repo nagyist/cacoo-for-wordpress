89632f0a5b9be9ffadc63ccd604f5d365df711fb
agattino <agattino@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-18 10:33:12 +0000

    Adding first version of Cacoo plugin for WordPress
    git-svn-id: https://plugins.svn.wordpress.org/cacoo-for-wordpress/trunk@174873 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ cacoo.php
@@ -0,0 +1,66 @@
+<?php
+/*
+Plugin Name: Cacoo for WordPress
+Plugin URI: http://cacoo.com/
+Description: draw graphics, charts, diagrams plugin.
+Author: Nulab Inc.
+Version: 0.1
+Author URI: http://www.nulab.co.jp/
+*/
+class Cacoo { 
+    function Cacoo() {
+        add_action('plugins_loaded', array(&$this, 'Initalization'));
+		// Register shortcodes
+		add_shortcode('cacoo', array(&$this, 'shortcode_cacoo') );
+		if ( is_admin() ) {
+			// Editor pages only
+			if ( in_array( basename($_SERVER['PHP_SELF']), apply_filters( 'vvq_editor_pages', array('post-new.php', 'page-new.php', 'post.php', 'page.php') ) ) ) {
+				wp_enqueue_script( 'jquery-ui-dialog' );
+				wp_enqueue_style( 'cacoo-jquery-ui', plugins_url('/cacoo/resources/cacoo-jquery-ui.css'), array(), $this->version, 'screen' );
+			}
+		}
+    }
+    function sink_hooks(){
+        add_filter('mce_plugins', array(&$this, 'mce_plugins'));
+    }
+    function Initalization() {
+        add_action('init', array(&$this, 'addbuttons'));
+    }
+    function addbuttons() {
+        // auth
+       if ( ! current_user_can('edit_posts') && ! current_user_can('edit_pages') )
+         return;
+    
+       // supported rich editor only 
+       if ( get_user_option('rich_editing') == 'true') {
+         add_filter("mce_external_plugins", array(&$this, 'mce_external_plugins'));
+         add_filter('mce_buttons', array(&$this, 'mce_buttons'));
+       }
+    }
+    function mce_buttons($buttons) {
+       // add button name
+       array_push($buttons, "separator", "Cacoo");
+       return $buttons;
+    }
+    // load TinyMCE plugin file :  editor_plugin.js (wp2.5)
+    function mce_external_plugins($plugin_array) {
+       //plugin function name
+       $plugin_array['Cacoo'] = plugins_url('/cacoo/resources/tinymce3/editor_plugin.js');
+       return $plugin_array;
+    }
+	// Handle Cacoo shortcodes
+	function shortcode_cacoo( $atts, $content = '' ) {
+      $width = $atts['width'] == null ? '410' : $atts['width'];
+      $height = $atts['height'] == null ? '330' : $atts['height'];
+      $ifwidth = $width + 20;
+      $ifheight = $height + 30;
+      $theme = $atts['theme'] == null ? 'cacoo' : $atts['theme'];
+      return
+        '<iframe src="' . $content . '/view?w=' . $width . '&h=' . $height . '&tm=' . $theme . '" width="'. $ifwidth . '" height="' . $ifheight . '" style="border:0px solid #fff">' .
+        '</iframe>'
+        ;
+	}
+}
+$cacoo = new Cacoo();
+add_action('init',array(&$cacoo, 'Cacoo'));
+?>
---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== Plugin Name ===
+Contributors: agattino
+Donate link: http://cacoo.com/
+Tags: cacoo, diagram, drawing, draw, flowchart, uml, Post, page, plugin, edit
+Requires at least: 2.8
+Tested up to: 2.8.4
+Stable tag: 0.1
+
+Cacoo is a user friendly online diagramming application. The Cacoo plugin for WordPress allows you to create diagrams and insert them into your posts.
+
+== Description ==
+
+The Cacoo plugin for WordPress works with your Cacoo account.
+
+== Installation ==
+
+1. Upload the entire `cacoo-for-wordpress` folder to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+
+You will find 'Cacoo' menu in your WordPress admin panel.
+
+== Frequently Asked Questions ==
+
+Please see [FAQ](http://cacoo.com/faq).
+
+If you have any further questions,
+please submit them [to the support](http://cacoo.com/support).
+
+Attention: [Check your site before reporting trouble, please!](http://cacoo.com/support)
+
+== Screenshots ==
+
+1. screenshot-1.png
+1. screenshot-2.png
+1. screenshot-3.png
+1. screenshot-4.png
+
+== Changelog ==
