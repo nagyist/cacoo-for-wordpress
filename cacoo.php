82e13e85b52c5fbb1891f974f71eb29acf66a427
agattino <agattino@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-18 17:17:58 +0000

    Updated version 0.2
    git-svn-id: https://plugins.svn.wordpress.org/cacoo-for-wordpress/trunk@175002 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cacoo.php
+++ cacoo.php
@@ -4,7 +4,7 @@ Plugin Name: Cacoo for WordPress
 Plugin URI: http://cacoo.com/
 Description: draw graphics, charts, diagrams plugin.
 Author: Nulab Inc.
-Version: 0.1
+Version: 0.2
 Author URI: http://www.nulab.co.jp/
 */
 class Cacoo { 
